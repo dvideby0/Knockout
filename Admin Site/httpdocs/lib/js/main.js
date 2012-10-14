2b11e2ee7a4bf7e2caba1516dc459e676aa7f58b
rbrookfield <rbrookfield@yahoo.com>
2012-10-14 16:56:28 -0400

    Added some sttic example charts

--- Admin Site/httpdocs/lib/js/main.js
+++ Admin Site/httpdocs/lib/js/main.js
@@ -1,10 +1,99 @@
-$('#SupportUsLink').attr('href', 'http://yearofthecu.com:3737?ClientID=12345678910&ReturnURL=' + location.href);
-
-function ShowModal(){
-    $('#myModal').modal({
-        backdrop: true
+var chart;
+$(document).ready(function(){
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart1',
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
     });
-}
-function HideModal(){
-    $('#myModal').modal('hide');
-}
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart2',
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart3',
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart4',
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
