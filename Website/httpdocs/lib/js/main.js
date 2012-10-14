a10b5abd7ed9bfbfff39ee7a073b0c1171f67719
rbrookfield <rbrookfield@yahoo.com>
2012-10-14 00:09:50 -0400

    put up a accept box

--- Website/httpdocs/lib/js/main.js
+++ Website/httpdocs/lib/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    document.getElementById('SupportUsLink').setAttribute('href', 'http://yearofthecu.com:3737?ClientID=12345678910&ReturnURL=' + location.href);
+    $('#SupportUsLink').attr('href', 'http://yearofthecu.com:3737?ClientID=12345678910&ReturnURL=' + location.href);
 });
+
+function ShowModal(){
+    $('#myModal').show();
+}
+function HideModal(){
+    $('#myModal').hide();
+}
