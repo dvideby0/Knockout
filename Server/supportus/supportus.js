c1cae399f1edad599db3ec1e3320edc9bf92f157
rbrookfield <rbrookfield@yahoo.com>
2012-10-13 21:35:22 -0400

    added appropriate plugins

--- Server/supportus/supportus.js
+++ Server/supportus/supportus.js
@@ -6,20 +6,42 @@ var express = require('express')
         , express.session({ secret: 'some secret' })
     );
 var aToken;
+var Mongolian = require("mongolian");
+var uuid = require('node-uuid');
+var server = new Mongolian;
+var db = server.db("supportus");
+var users = db.collection("users");
 app.use(express.bodyParser());
 app.get('/', function (req, res) {
     res.redirect(fb.getAuthorizeUrl({
         client_id: '359603454132277',
         redirect_uri: 'http://yearofthecu.com:3737/auth?ClientID=' + req.query.ClientID,
-        scope: 'offline_access,publish_stream'
+        scope: 'offline_access,publish_stream',
+        state: req.query.ReturnURL
     }));
 });
 
 app.get('/auth', function (req, res) {
     fb.getAccessToken('359603454132277', '43999899ea27f624240f77739579b34c', req.param('code'), 'http://yearofthecu.com:3737/auth?ClientID=' + req.query.ClientID, function (error, access_token, refresh_token) {
-        res.send('<script type="text/javascript">window.location = "http://facebook.com/home.php"</script>');
-        console.log(access_token + '\n' + req.query.ClientID);
+        res.send('<script type="text/javascript">window.location = "' + req.query.state + '"</script>');
         aToken = access_token;
+        fb.apiCall('GET', '/me',
+            {access_token: aToken, message: req.param('message')},
+            function (error, response, body) {
+                console.log(body.id);
+                users.findOne({ID: body.id}, function(err, post){
+                    if(!post){
+                        users.insert({
+                            ID: body.id,
+                            ClientID: req.query.ClientID,
+                            AccessToken: access_token
+                        });
+                    }
+                });
+            }
+        );
+        console.log(access_token + '\n' + req.query.ClientID + '\n' + req.query.state);
+
     });
 });
 
