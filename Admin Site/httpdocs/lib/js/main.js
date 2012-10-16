e3fe3996817005f512542abc347c876dd9b32cf7
rbrookfield <rbrookfield@yahoo.com>
2012-10-16 00:50:30 -0400

    added server functionality and cleaned up functions css and files

--- Admin Site/httpdocs/lib/js/main.js
+++ Admin Site/httpdocs/lib/js/main.js
@@ -46,9 +46,44 @@ $(document).ready(function(){
 
 //-------------------------------Generate Charts-----------------------------------------
 
-    var chart = new Highcharts.Chart({
+    chart = new Highcharts.Chart({
         chart: {
             renderTo: 'chart1',
+            type: 'area',
+            marginBottom: 70
+        },
+        title: {
+            text: 'Total: '
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Count'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.series.name +'</b><br/>'+
+                    this.x +': '+ this.y +'°C';
+            }
+        },
+        series: [{
+            name: 'Supporters',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
+    });
+
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart2',
             plotBackgroundColor: null,
             plotBorderWidth: null,
             plotShadow: false
@@ -95,32 +130,8 @@ $(document).ready(function(){
     });
     var chart = new Highcharts.Chart({
         chart: {
-            renderTo: 'chart2',
-            type: 'bar'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 3, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
-    var chart = new Highcharts.Chart({
-        chart: {
             renderTo: 'chart3',
-            type: 'bar'
+            type: 'column'
         },
         title: {
             text: 'Fruit Consumption'
@@ -144,7 +155,7 @@ $(document).ready(function(){
     var chart = new Highcharts.Chart({
         chart: {
             renderTo: 'chart4',
-            type: 'column'
+            type: 'bar'
         },
         title: {
             text: 'Fruit Consumption'
@@ -168,7 +179,7 @@ $(document).ready(function(){
     var chart = new Highcharts.Chart({
         chart: {
             renderTo: 'chart5',
-            type: 'column'
+            type: 'line'
         },
         title: {
             text: 'Fruit Consumption'
--- Server/supportus/supportus.js
+++ Server/supportus/supportus.js
@@ -5,18 +5,18 @@ var express = require('express')
         , express.cookieParser()
         , express.session({ secret: 'some secret' })
     );
-var aToken;
 var Mongolian = require("mongolian");
 var uuid = require('node-uuid');
 var server = new Mongolian;
 var db = server.db("supportus");
 var users = db.collection("users");
+var usersfriends = db.collection("usersfriends");
 app.use(express.bodyParser());
 app.get('/', function (req, res) {
     res.redirect(fb.getAuthorizeUrl({
         client_id: '359603454132277',
         redirect_uri: 'http://yearofthecu.com:3737/auth?ClientID=' + req.query.ClientID,
-        scope: 'offline_access,publish_stream,email,user_likes, friends_about_me',
+        scope: 'user_about_me,user_actions.music,user_actions.news,user_actions.video,user_activities,user_education_history,user_events,user_games_activity,user_groups,user_hometown,user_interests,user_notes,user_photos,user_questions,user_relationship_details,user_relationships,user_religion_politics,user_status,user_subscriptions,user_videos,user_website,user_work_history,friends_actions.music,friends_actions.news,friends_actions.video,friends_activities,friends_birthday,friends_education_history,friends_events,friends_games_activity,friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,friends_notes,friends_photos,friends_questions,friends_relationship_details,friends_relationships,friends_religion_politics,friends_status,friends_subscriptions,friends_videos,friends_website,friends_work_history,ads_management,create_event,export_stream,friends_online_presence,manage_friendlists,manage_notifications,manage_pages,offline_access,publish_checkins,read_friendlists,read_insights,read_mailbox,read_page_mailboxes,read_requests,read_stream,rsvp_event,sms,user_online_presence,xmpp_login',
         state: req.query.ReturnURL
     }));
 });
@@ -25,43 +25,56 @@ app.get('/auth', function (req, res) {
     fb.getAccessToken('359603454132277', '43999899ea27f624240f77739579b34c', req.param('code'), 'http://yearofthecu.com:3737/auth?ClientID=' + req.query.ClientID, function (error, access_token, refresh_token) {
         res.setHeader('content-type', 'text/html');
         res.send('<html><head><script type="text/javascript">window.location = "' + decodeURI(req.query.state) + '"; </script></head></html>');
-        aToken = access_token;
+        var aToken = access_token;
         fb.apiCall('GET', '/me',
-            {fields: 'id,email,gender,name', access_token: aToken},
+            {fields: 'id,email,gender,name,birthday,location', access_token: aToken},
             function (error, response, body) {
-                console.log(body);
                 users.findOne({ID: body.id}, function(err, post){
                     if(!post){
+                        var bdayArr = body.birthday.split('/');
+                        var d = new Date(bdayArr[2], bdayArr[0], bdayArr[1]);
+                        var today = new Date();
                         users.insert({
                             Name: body.name,
                             Email: body.email,
                             Gender: body.gender,
+                            DOB: d.getTime()/1000.0,
+                            Location: body.location,
                             ID: body.id,
                             ClientID: req.query.ClientID,
+                            Date: parseInt(today.getTime()/1000.0),
                             AccessToken: access_token
                         });
+                        fb.apiCall('GET', '/me',
+                            {fields: 'posts.limit(100)', access_token: aToken},
+                            function(error, response, body){
+                                var likes = 0;
+                                var comments = 0;
+                                for(var i = 0; i < body.posts.data.length; i++){
+                                    if(body.posts.data[i].likes){
+                                        likes = likes + body.posts.data[i].likes.count;
+                                    }
+                                    comments = comments + body.posts.data[i].comments.count;
+                                    if((i+1) == body.posts.data.length){
+                                        likes = likes/100;
+                                        comments = comments/100;
+                                        users.update({ID: body.id}, {"$addToSet": {LikesAvg:likes, CommentsAvg: comments}});
+                                    }
+                                }
+                            });
                     }
                 });
             }
         );
-        console.log(access_token + '\n' + req.query.ClientID + '\n' + req.query.state);
-
     });
 });
 
-app.post('/message', function (req, res) {
-    fb.apiCall('POST', '/me/feed',
-        {access_token: aToken, message: req.param('message')},
-        function (error, response, body) {
-            res.send({body: body});
-        }
-    );
-});
-
-
-app.get('/messages', function (req, res) {
-    var stream = fb.apiCall('GET', '/me/feed', {access_token: req.param('access_token'), message: req.param('message')});
-    stream.pipe(fs.createWriteStream('backup_feed.txt'));
+app.get('/SupportersCount', function(req, res){
+    console.log()
+    users.count({ClientID: req.query.ClientID}, function(err, post){
+        console.log(post);
+        res.send({"count": post})
+    });
 });
 
 app.listen(3737);
